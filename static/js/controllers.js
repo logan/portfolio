bc0d15efab93e25d985267fef7c6fa3da7cc95ec
Logan Hanks <intortus@reddit.com>
2014-02-21 16:25:24 -0800

    Add analytics support.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,5 +1,12 @@
 angular.module('loganControllers', [])
 
+.controller('loganMainCtrl',
+    function($scope, $location, $window) {
+        $scope.$on('$viewContentLoaded', function(event) {
+            ga('send', 'pageview', {'page': $location.path()})
+        })
+    })
+
 .controller('loganFrontCtrl',
     function($scope, $window, $templateCache, loganProjects) {
         // TODO: remove
